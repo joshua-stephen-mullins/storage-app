24c53e6435c3dc62d54902b46a81c4187da00589
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2023-10-21 13:22:16 -0500

    Created Project

---
+++ src/test/java/joshua/storageapp/StorageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package joshua.storageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
